3c4c0c1979ae7a1b3f35ee8d00abc6e454187570
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-10-29 03:02:37 -0300

    Implementando NavLink en el Header

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,9 +1,11 @@
+import { NavLink } from "react-router-dom"
+
 function Navbar() {
     return (
         <div className="self-center flex items-center justify-center space-x-4 text-xl font-medium order-last flex-basis-[100%] md:order-none relative z-10">
-            <a href="/mix?format=hex" className="">About Me</a>
-            <a href="/blend?format=hex" className="">Proyectos</a>
-            <a href="/modify?format=hex" className="">Educación</a>
+            <NavLink to="/sobremi" className={({ isActive }) => isActive ? "text-[#d12c4f] dark:text-[#ff1f4f]" : ""}>Sobre Mi</NavLink>
+            <NavLink to="/proyectos" className={({ isActive }) => isActive ? "text-[#d12c4f] dark:text-[#ff1f4f]" : ""}>Proyectos</NavLink>
+            <NavLink to="/educacion" className={({ isActive }) => isActive ? "text-[#d12c4f] dark:text-[#ff1f4f]" : ""}>Educación</NavLink>
         </div>
     )
 }
