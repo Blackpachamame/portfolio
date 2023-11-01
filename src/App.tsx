401dc2785339f736bb8dea5b6862dd8e2ea8c88e
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-01 00:31:35 -0300

    Corregidos colores navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ function App() {
   return (
     <BrowserRouter>
       <main className="min-h-screen flex flex-col p-4 md:p-6">
-        <div className="fixed inset-0 bg-[#f5f1e7] dark:bg-[#11121b]"></div>
+        <div className="fixed inset-0 bg-[rgba(68,61,127,.1)] dark:bg-[#11121b]"></div>
         <Header></Header>
         <div className="flex-1 flex flex-col justify-center relative z-10 
         bg-gradient-to-tr from-[rgba(138,60,60,.2)] to-transparent text-gray-900 
         dark:from-[rgba(78,89,192,.2)] dark:text-[#f5f1e7] 
-        border border-solid border-[#ffffff] dark:border-[#242636] rounded-2xl 
+        border border-solid border-[#e2d9df] dark:border-[#242636] rounded-2xl 
         shadow-lg p-6 md:p-10 pb-8 md:pb-12 transition duration-300">
           <Routes>
             <Route path="/" element={<Home />} />
