28ccc6b49f6e59715d36ce58a2cf3f281995af3a
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-02 19:01:08 -0300

    Ajustes modo claro

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,12 @@ function App() {
   return (
     <BrowserRouter>
       <main className="min-h-screen flex flex-col p-4 md:p-6">
-        <div className="fixed inset-0 bg-[rgba(68,61,127,.1)] dark:bg-[#11121b]"></div>
+        <div className="fixed inset-0 bg-[rgba(60,62,138,.1)] dark:bg-[#11121b]"></div>
         <Header></Header>
         <div className="flex-1 flex flex-col justify-center relative z-10 
-        bg-gradient-to-tr from-[rgba(138,60,60,.2)] to-transparent text-gray-900 
+        bg-gradient-to-tr from-[rgba(60,62,138,.2)] to-transparent text-gray-900 
         dark:from-[rgba(78,89,192,.2)] dark:text-slate-100
-        border border-solid border-[#e2d9df] dark:border-[#242636] rounded-2xl 
+        border border-solid border-[#d9dae2] dark:border-[#242636] rounded-2xl 
         shadow-lg p-6 md:p-10 pb-8 md:pb-12 transition duration-300">
           <Routes>
             <Route path="/" element={<Home />} />
--- src/components/Home/ButtonCV.tsx
+++ src/components/Home/ButtonCV.tsx
@@ -3,8 +3,8 @@ import { BsDownload } from "react-icons/bs";
 function ButtonCV() {
     return (
         <a href="#" className="inline-flex items-center gap-2 px-4 py-2 sm:px-5 sm:py-3
-        font-semibold text-white bg-[#865b69] dark:bg-indigo-600 dark:border-[#242636] 
-        rounded-full cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-rose-800
+        font-semibold text-gray-950 dark:text-white bg-indigo-300 dark:bg-indigo-600 dark:border-[#242636] 
+        rounded-full cursor-pointer select-none sm:mb-0 sm:w-auto hover:bg-indigo-400
         dark:hover:bg-indigo-700 transition duration-300">
             Curriculum
             <BsDownload />
