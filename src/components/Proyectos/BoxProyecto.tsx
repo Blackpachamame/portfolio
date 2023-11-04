010aa46ec2b1058992592bcc31a9db4a27ec080e
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-04 04:33:23 -0300

    Avanzando con estilos boxproyecto, aplicando transition a las imagenes.

--- src/components/Proyectos/BoxProyecto.tsx
+++ src/components/Proyectos/BoxProyecto.tsx
@@ -1,7 +1,22 @@
 function BoxProyecto() {
     return (
-        <div className="flex flex-col items-center border border-solid border-[#d9dae2] dark:border-[#242636] rounded-2xl
-        bg-gradient-to-tr from-[rgba(60,62,138,.2)] to-transparent p-4">BoxProyecto</div>
+        <div className="flex flex-col items-center border border-solid border-[#d9dae2] dark:border-[#292b3d] rounded-2xl
+        bg-gradient-to-t from-[rgba(124,130,177,.12)] to-[rgba(17,18,27,0)] p-4">
+            <figure className="rounded-lg overflow-hidden">
+                <img className="w-full scale-100 hover:scale-125 transition duration-300 ease-in-out"
+                    src="https://blackpachamame.github.io/desafíos-frontendmentor/testimonials-grid-section/design/desktop-preview.jpg"
+                    alt="newsletter preview" />
+            </figure>
+            <div className="text-center p-3">
+                <h3>Testimonials grid section</h3>
+                <h4>
+                    <a
+                        href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/">Sitio
+                        en Vivo
+                    </a>
+                </h4>
+            </div>
+        </div>
     )
 }
 export default BoxProyecto
