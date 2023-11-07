0a73917417146ac37549243f6dd0a0e9ee98a4cb
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-07 02:47:59 -0300

    Ahora los enlaces del proyecto están separados del titulo

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -2,11 +2,11 @@ import { NavLink } from "react-router-dom"
 
 function Navbar() {
     return (
-        <div className="self-center flex items-center justify-center gap-4 font-semibold order-last flex-basis-[100%] md:order-none relative z-10 text-sm sm:text-xl">
+        <nav className="min-h-[32px] flex justify-center items-center gap-4 font-semibold order-last flex-basis-[100%] md:order-none relative z-10 text-sm sm:text-xl">
             <NavLink to="/sobremi" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Sobre mi</NavLink>
             <NavLink to="/proyectos" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Proyectos</NavLink>
             <NavLink to="/educacion" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Educación</NavLink>
-        </div>
+        </nav>
     )
 }
 export default Navbar
--- src/components/Proyectos/BoxProyecto.tsx
+++ src/components/Proyectos/BoxProyecto.tsx
@@ -3,23 +3,23 @@ import { FiGithub } from "react-icons/fi";
 
 function BoxProyecto() {
     return (
-        <div className="flex flex-col items-center border border-solid border-[#d9dae2] dark:border-[#292b3d] rounded-2xl
+        <div className="flex flex-col justify-between items-center border border-solid border-[#d9dae2] dark:border-[#292b3d] rounded-2xl
         bg-gradient-to-t from-[rgba(124,130,177,.12)] to-[rgba(17,18,27,0)] overflow-hidden">
-            <figure className="rounded-lg overflow-hidden">
+            <figure className="overflow-hidden">
                 <img className="w-full scale-100 hover:scale-125 transition duration-300 ease-in-out"
                     src="https://blackpachamame.github.io/desafíos-frontendmentor/testimonials-grid-section/design/desktop-preview.jpg"
                     alt="newsletter preview" />
             </figure>
-            <div className="flex flex-col gap-3 text-center font-semibold p-4">
+            <div className="w-full text-center font-semibold p-4 pb-2">
                 <h3 className="text-xl">Testimonials grid section</h3>
-                <div className="flex justify-center gap-4">
-                    <a className="flex items-center gap-2"
-                        href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><FaArrowUpRightFromSquare style={{ fontSize: "16px" }} />Online
-                    </a>
-                    <a className="flex items-center gap-2"
-                        href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><FiGithub style={{ fontSize: "16px" }} />Código
-                    </a>
-                </div>
+            </div>
+            <div className="w-full flex justify-center gap-4 p-4 pt-2">
+                <a className="flex items-center gap-2"
+                    href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><FaArrowUpRightFromSquare style={{ fontSize: "16px" }} />Online
+                </a>
+                <a className="flex items-center gap-2"
+                    href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><FiGithub style={{ fontSize: "16px" }} />Código
+                </a>
             </div>
         </div>
     )
