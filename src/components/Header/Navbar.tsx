f4bf9158e5d52ac64469f2ff483f2c44737faf89
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-07 01:24:16 -0300

    Ajustando iconos proyectos

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 function Navbar() {
     return (
-        <div className="self-center flex items-center justify-center space-x-4 font-semibold order-last flex-basis-[100%] md:order-none relative z-10 text-sm sm:text-xl">
+        <div className="self-center flex items-center justify-center gap-4 font-semibold order-last flex-basis-[100%] md:order-none relative z-10 text-sm sm:text-xl">
             <NavLink to="/sobremi" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Sobre mi</NavLink>
             <NavLink to="/proyectos" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Proyectos</NavLink>
             <NavLink to="/educacion" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Educación</NavLink>
--- src/components/Proyectos/BoxProyecto.tsx
+++ src/components/Proyectos/BoxProyecto.tsx
@@ -1,24 +1,25 @@
-import { TbBrandGithub, TbEye } from "react-icons/tb";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6"; FiGithub
+import { FiGithub } from "react-icons/fi";
 
 function BoxProyecto() {
     return (
         <div className="flex flex-col items-center border border-solid border-[#d9dae2] dark:border-[#292b3d] rounded-2xl
-        bg-gradient-to-t from-[rgba(124,130,177,.12)] to-[rgba(17,18,27,0)] p-4">
+        bg-gradient-to-t from-[rgba(124,130,177,.12)] to-[rgba(17,18,27,0)] overflow-hidden">
             <figure className="rounded-lg overflow-hidden">
                 <img className="w-full scale-100 hover:scale-125 transition duration-300 ease-in-out"
                     src="https://blackpachamame.github.io/desafíos-frontendmentor/testimonials-grid-section/design/desktop-preview.jpg"
                     alt="newsletter preview" />
             </figure>
-            <div className="text-center font-semibold p-3">
+            <div className="flex flex-col gap-3 text-center font-semibold p-4">
                 <h3 className="text-xl">Testimonials grid section</h3>
-                <h4>
-                    <a
-                        href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><TbEye /> Sitio
-                        en Vivo
+                <div className="flex justify-center gap-4">
+                    <a className="flex items-center gap-2"
+                        href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><FaArrowUpRightFromSquare style={{ fontSize: "16px" }} />Online
                     </a>
-
-                    <TbBrandGithub />
-                </h4>
+                    <a className="flex items-center gap-2"
+                        href="https://blackpachamame.github.io/desafíos-frontendmentor/newsletter-sign-up-with-success-message/"><FiGithub style={{ fontSize: "16px" }} />Código
+                    </a>
+                </div>
             </div>
         </div>
     )
