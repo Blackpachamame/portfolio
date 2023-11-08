c473650671f9bb309a610982cf49ac975c255276
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-07 21:46:05 -0300

    Corregido navbar, no me dejaba clickear redes ni home

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 
 function Navbar() {
     return (
-        <nav className="min-h-[32px] flex justify-center items-center gap-4 font-semibold order-last flex-basis-[100%] md:order-none relative z-10 text-sm sm:text-xl">
+        <nav className="min-h-[32px] self-center flex justify-center items-center gap-4 font-semibold order-last flex-basis-[100%] md:order-none relative z-10 text-sm min-[375px]:text-lg sm:text-xl">
             <NavLink to="/sobremi" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Sobre mi</NavLink>
             <NavLink to="/proyectos" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Proyectos</NavLink>
             <NavLink to="/educacion" className={({ isActive }) => isActive ? "text-indigo-800 dark:text-white duration-500" : "hover:text-indigo-900 dark:hover:text-slate-200 duration-500"}>Educación</NavLink>
