a23ed49da4a3bd7a3f4222b8497fe9dd2003469a
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-08 11:20:42 -0300

    Creando tooltips skills

--- src/components/Proyectos/Skills.tsx
+++ src/components/Proyectos/Skills.tsx
@@ -1,6 +1,22 @@
+import { SiReact } from "react-icons/si";
+
 function Skills() {
     return (
-        <div>Skills</div>
+        <div className="flex flex-wrap gap-5">
+            <div className="group relative inline-block">
+                <button className="w-16 h-16 flex justify-center items-center
+                bg-[rgba(60,62,138,.2)] dark:bg-[#11121b] border border-solid border-[#d9dae2] dark:border-[#242636]
+                rounded-2xl shadow-lg">
+                    <SiReact style={{ fontSize: "32px" }} />
+                </button>
+                <div className="bg-[#BFBFD9] dark:bg-[#11121b] absolute top-full left-1/2 z-20 mt-3 
+                -translate-x-1/2 rounded-[5px] py-1.5 px-3.5 text-center text-sm leading-4 font-semibold 
+                opacity-100 group-hover:opacity-100 transition duration-300">
+                    <span className="bg-[#BFBFD9] dark:bg-[#11121b] absolute top-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45"></span>
+                    Styled Componets
+                </div>
+            </div>
+        </div>
     )
 }
 export default Skills
--- src/components/Proyectos/index.tsx
+++ src/components/Proyectos/index.tsx
@@ -1,11 +1,15 @@
 import BoxProyecto from "./BoxProyecto";
+import Skills from "./Skills";
 
 function Proyectos() {
   return (
-    <div className="grid justify-center items-stretch proyecto__container gap-4">
-      <BoxProyecto />
-      <BoxProyecto />
-      <BoxProyecto />
+    <div className="flex flex-col items-center gap-12">
+      <Skills />
+      <div className="grid justify-center items-stretch proyecto__container gap-4">
+        <BoxProyecto />
+        <BoxProyecto />
+        <BoxProyecto />
+      </div>
     </div>
   );
 }
