8010eead9536b37a2ce47bd3e815556b8585a54e
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-18 02:43:56 -0300

    Probando con bordes al rededor del logo del proyecto

--- src/components/Proyectos/BoxProyectos.tsx
+++ src/components/Proyectos/BoxProyectos.tsx
@@ -16,13 +16,15 @@ export default function BoxProyectos({ data }: EmployeeProps) {
     return (
         <div className="flex flex-col justify-between items-center border border-solid border-[#d9dae2] dark:border-[#292b3d] rounded-2xl
         bg-gradient-to-t from-[rgba(124,130,177,.12)] to-[rgba(17,18,27,0)] overflow-hidden">
-            <figure className="overflow-hidden">
+            <figure className="relative overflow-hidden">
                 <img className="w-full scale-100 hover:scale-125 transition duration-300 ease-in-out"
                     src={data.imgProyecto}
                     alt={"Imagen previa del proyecto " + data.nombre} />
+                <div className="w-12 h-12 border-2 border-[#11121b] absolute bottom-0 right-2/4 translate-x-[50%] translate-y-[24px] z-10 flex justify-center items-center bg-[#d0d1e3] dark:bg-[#1e2137] rounded-full"></div>
+                <div className="w-full h-0.5 absolute bottom-0 right-0 flex bg-[#11121b]"></div>
             </figure>
             <div className="relative w-full flex flex-col justify-between items-center grow gap-4 p-4 pt-8">
-                <div className="w-12 h-12 absolute top-0 right-2/4 translate-x-[50%] translate-y-[-24px] z-10 flex justify-center items-center bg-[rgb(243,243,243)] dark:bg-[#11121b] rounded-full shadow-lg">
+                <div className="w-12 h-12 absolute top-0 right-2/4 translate-x-[50%] translate-y-[-24px] z-10 flex justify-center items-center bg-[transparent] rounded-full">
                     <figure className="w-8 h-8 rounded-full overflow-hidden">
                         <img src={data.logo} alt={"Logo proyecto " + data.nombre} width="32" height="32" />
                     </figure>
