60259a7a7955d356004d6fddbd3346c9ff6c50c5
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-11-21 02:03:03 -0300

    Agrupando hover de la descripcion del proyecto

--- src/components/Proyectos/BoxProyectos.tsx
+++ src/components/Proyectos/BoxProyectos.tsx
@@ -16,10 +16,17 @@ export default function BoxProyectos({ data }: EmployeeProps) {
     return (
         <div className="flex flex-col justify-between items-center border border-solid border-[#d9dae2] dark:border-[#292b3d] rounded-2xl
         overflow-hidden">
-            <div className="relative overflow-hidden">
-                <img className="w-full scale-100 hover:scale-125 transition duration-300 ease-in-out"
+            <div className="group relative overflow-hidden">
+                <img className="w-full scale-100 group-hover:scale-125 transition duration-300 ease-in-out"
                     src={data.imgProyecto}
                     alt={"Imagen previa del proyecto " + data.nombre} />
+                {/* Descripcion proyecto */}
+                <div className="absolute inset-0 z-10 bg-[rgba(60,62,138,.9)] opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    <div className="w-full h-full flex justify-center items-center">
+                        <p className="">Hola Carola</p>
+                    </div>
+                </div>
+                {/* Borde arriba del logo */}
                 <div className="w-12 h-12 absolute bottom-0 right-2/4 translate-x-[50%] translate-y-[24px] z-10 flex justify-center items-center bg-[rgb(208,209,227)] dark:bg-[rgb(23,25,44)] rounded-full"></div>
                 {/* Borde izquierdo del logo */}
                 <div className="absolute w-4 h-4 bottom-0 right-[50%] translate-x-[-23px] z-10 bg-transparent shadow-[8px_8px_0px_rgb(208,209,227)] dark:shadow-[8px_8px_0px_rgb(23,25,44)] rounded-full"></div>
