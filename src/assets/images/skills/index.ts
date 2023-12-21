16d6300d42c0bf10dd34ed47d385db103da99be8
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-12-21 11:05:49 -0300

    feat: Agregando nuevas skills fix: Corriendo pointer events de las tooltip de las skills

--- src/assets/images/skills/index.ts
+++ src/assets/images/skills/index.ts
@@ -7,6 +7,13 @@ import sass from "../skills/logo-sass.svg";
 import styledComponents from "../skills/logo-styled-components.svg";
 import tailwind from "../skills/logo-tailwind.svg";
 import typescript from "../skills/logo-typescript.svg";
+import figma from "../skills/logo-figma.svg";
+import git from "../skills/logo-git.svg";
+import github from "../skills/logo-github.svg";
+import jquery from "../skills/logo-jquery.svg";
+import nodejs from "../skills/logo-nodejs.svg";
+import mysql from "../skills/logo-mysql.svg";
+import photoshop from "../skills/logo-photoshop.svg";
 
 export {
     bootstrap,
@@ -17,4 +24,11 @@ export {
     styledComponents,
     tailwind,
     typescript,
+    figma,
+    git,
+    github,
+    jquery,
+    nodejs,
+    mysql,
+    photoshop,
 };
--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -1,7 +1,24 @@
+import { skills } from "../../assets/data";
+
 export default function Skills() {
+
   return (
-    <div>
-      <h2>Skills</h2>
+    <div className="flex justify-center flex-wrap gap-5">
+      {skills.map((skill) => (
+        <div key={skill.id} className="group relative inline-block">
+          <figure
+            className="w-14 h-14 md:w-16 md:h-16 flex justify-center items-center bg-[rgba(243,243,243,.3)] dark:bg-[#11121b] 
+                        border border-solid border-[#d9dae2] dark:border-[#242636] rounded-2xl shadow-lg transition duration-300">
+            <img src={skill.imgSkill} width="32px" height="32px" alt={"Logo " + skill.nombre} />
+          </figure>
+          <div className="bg-[#BFBFD9] dark:bg-[#11121b] absolute top-full left-1/2 z-20 pointer-events-none mt-3 
+                -translate-x-1/2 rounded-[5px] py-1.5 px-3.5 text-center text-sm leading-4 font-semibold 
+                opacity-0 group-hover:opacity-100 transition duration-300">
+            <span className="bg-[#BFBFD9] dark:bg-[#11121b] absolute top-[-3px] left-1/2 -z-10 h-2 w-2 -translate-x-1/2 rotate-45"></span>
+            {skill.nombre}
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
