9a5a1acade1ffe61c612c7c8bad573dc2d452d94
Marcos Travaglini <marcos.travaglini@est.fi.uncoma.edu.ar>
2023-12-26 00:55:02 -0300

    feat: Aplicado persistencia de elección de theme

--- src/components/Header/Brand.tsx
+++ src/components/Header/Brand.tsx
@@ -5,23 +5,27 @@ import logoBlack from "./../../assets/logoDark.png"
 import { NavLink } from "react-router-dom";
 
 export default function Brand() {
-    const [theme, setTheme] = useState(() => {
-        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-            return "dark";
-        }
-        return "light";
-    });
+    const themePrefers = window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light";
+    const [theme, setTheme] = useState(getInitialTheme);
 
     useEffect(() => {
         if (theme === "dark") {
-            document.querySelector("html")!.classList.add("dark");
+            document.querySelector("html")?.classList.add("dark");
         } else {
-            document.querySelector("html")!.classList.remove("dark");
+            document.querySelector("html")?.classList.remove("dark");
         }
+        localStorage.setItem("theme", JSON.stringify(theme));
     }, [theme]);
 
+    function getInitialTheme() {
+        const savedTheme = localStorage.getItem("theme");
+        return savedTheme ? JSON.parse(savedTheme) : themePrefers;
+    }
+
     const handleChangeTheme = () => {
-        setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+        setTheme((prevTheme: string) => (prevTheme === "light" ? "dark" : "light"));
     };
 
     return (
